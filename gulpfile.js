3ad1e78775fcddfae264cdc08f0490353987b6a9
Steryos <bstergios@gmail.com>
2022-04-12 20:19:52 +0300

    added a gulpfile.js including 2 functions

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+var gulp = require('gulp');
+
+gulp.task('default', () => {
+  console.log("Hoorray - you created a Gulp task.");
+});
+
+gulp.task('html', () => {
+  console.log("Imagine something useful being done to your HTML here.");
+});
+
